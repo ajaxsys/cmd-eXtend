2e19983ed34b2ba5b110e8408598921c25153c1c
Andy.F <admin@ajaxsys.net>
2014-08-27 11:34:57 +0900

    Add file extention check hint

--- x-java/groovy-script/ShiftJISUTF8.groovy
+++ x-java/groovy-script/ShiftJISUTF8.groovy
@@ -5,5 +5,6 @@ def dir = 'D:/shiftjs'
 
 new File(dir).eachFileRecurse{
     // it.getText() supports UTF-8 & default system encoidng
+    // Check extention: it.getName().endsWith('.txt')
     if (it.isFile()) it.setText(it.getText(),"UTF-8");
 }
